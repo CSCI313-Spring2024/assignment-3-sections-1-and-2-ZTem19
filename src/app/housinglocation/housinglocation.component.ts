18a4473622dde22a1adc402e86614183aa13753a
Zander <zander.templin@gmail.com>
2025-03-04 13:42:24 -0600

    Added housing location component

---
+++ src/app/housinglocation/housinglocation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  templateUrl: './housinglocation.component.html',
+  styleUrl: './housinglocation.component.css',
+})
+export class HousinglocationComponent {}
