81b43b738e8d7de3affe339bc3b5fb384642c015
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 <zander.templin@gmail.com>
2025-03-17 13:40:54 -0500

    Integreated details page into the application

--- src/app/housinglocation/housinglocation.component.ts
+++ src/app/housinglocation/housinglocation.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housinglocation';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './housinglocation.component.html',
   styleUrl: './housinglocation.component.css',
 })
